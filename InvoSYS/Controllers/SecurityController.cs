f923d0d0745c165162ceee9dd9c4cc7fe6d5924b
Asad <asadansari1047@gmail.com>
2023-07-03 00:51:27 +0530

    Add project files.

---
+++ InvoSYS/Controllers/SecurityController.cs
@@ -0,0 +1,30 @@
+﻿using InvoSYS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InvoSYS.Controllers
+{
+    public class SecurityController : Controller
+    {
+        Inventory_managementEntities db = new Inventory_managementEntities();
+
+        // GET: Security
+
+
+        public ActionResult Login()
+        {
+            return View("Login");
+        }
+
+        public ActionResult Logout()
+        {
+            
+            return RedirectToAction("Login");
+        }
+
+    }
+}
